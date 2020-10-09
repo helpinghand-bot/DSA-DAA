690720574e6d6646439a453e20d82a633c03804f
Tanuj Mehta <tanujm241@gmail.com>
2020-10-09 10:06:43 +0530

    Update stack_using_queue.c
    Re-formatted the code and fixed some bugs, its more readable now.

--- stack_using_queue.c
+++ stack_using_queue.c
@@ -3,66 +3,47 @@
 #define size 10000
 int queue[size];
 int front=-1,rear=-1;
-void push(int val)
-{
-	if(front==-1)
-		front=0;
+void push(int val){
+	if(front==-1) front=0;
 	queue[++rear]=val;
-
 }
-void pop()
-{
-	
-	if(front==-1)
-		printf("Stack empty\n\n");
-	else if(front<=rear)
-		{
+
+void pop(){
+	if(front==-1) printf("Stack empty\n\n");
+	else if(front<=rear){
 		int x=rear-front,i;
-		for(i=0;i<x;i++)
-		{
+		for(i=0;i<x;i++){
 			push(queue[front++]);
 		}
 		printf("Popped value : %d",queue[front++]);
-		}
-	else
-		printf("Stack empty\n\n");
+		} else printf("Stack empty\n\n");
 }
-void display()
-{
+
+void display(){
 	int i;
-	
-	if(front==-1)
-		printf("Stack empty\n\n");
+	if(front==-1) printf("Stack empty\n\n");
 	else if(front<=rear)
 		for(i=front;i<=rear;i++)
 			printf("%d ",queue[i]);
-	else
-		printf("Stack empty\n\n");
+		else printf("Stack empty\n\n");
 }
-void push1()
-{
+void push1(){
 	int n,val;
 	printf("How many elements you want to push : ");
 	scanf("%d",&n);
 	printf("Enter elemnents : ");
-	while(n--)
-	{
+	while(n--){
 		scanf("%d",&val);
 		push(val);
-	}
-	
-		
-	
+	}	
 }
-int main()
-{
+
+int main(){
 	int choice;
 	do{
 		printf("\n1.Push\n2.Pop\n3.Display\n4.Quit\n\nChoice : ");
-		
 		scanf("%d",&choice);
-		switch(choice)
-		{
+		switch(choice){
 			case 1: push1();
 					system("cls");
 					printf("Elements pushed up successfully");
